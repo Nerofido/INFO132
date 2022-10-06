6d813508ebed31c45ca0b5f004d659108e4dcd0c
Nerofido <olstad22@gmail.com>
2022-10-07 00:05:01 +0200

    Delete overengineerdcurrency.py

--- overengineerdcurrency.py
+++
@@ -1,9 +0,0 @@
-# Where USD is the base currency you want to use
-url = "https://data.norges-bank.no/api/data/EXR/B.USD.NOK.SP?format=sdmx-json&lastNObservations=60&locale=en"
-
-# Making our request
-response = requests.get(url)
-data = response.json()
-
-# Your JSON object
-print (data)
